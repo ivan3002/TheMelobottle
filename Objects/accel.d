d708eb8dc9eea9c9ff5d0907fb01a8cb28bc6af2
Ivan Agyapong <ipa506@its.york.ac.uk>
2023-12-04 16:47:12 +0000

    5th commit, i2c to be tested

---
+++ Objects/accel.d
@@ -0,0 +1,9 @@
+./objects/accel.o: src\Sensors\accel.c \
+  C:\KEIL_V5\ARM\PACK\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\STM32F407xx.h \
+  C:\KEIL_V5\ARM\PACK\ARM\CMSIS\5.8.0\CMSIS\Core\Include\core_cm4.h \
+  c:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\KEIL_V5\ARM\PACK\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\KEIL_V5\ARM\PACK\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\KEIL_V5\ARM\PACK\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\KEIL_V5\ARM\PACK\ARM\CMSIS\5.8.0\CMSIS\Core\Include\mpu_armv7.h \
+  C:\KEIL_V5\ARM\PACK\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h
