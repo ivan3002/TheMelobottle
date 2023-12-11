185074cb165189281f30a2779c0a147d2c5a9f43
Ivan Agyapong <ipa506@its.york.ac.uk>
2023-12-11 17:07:27 +0000

    7th commit, i2c still not working

--- src/Sensors/accel.c
+++ src/Sensors/accel.c
@@ -6,20 +6,27 @@ int acceldataread(){
 	I2C1->CR1 |= I2C_CR1_START; // Send the I2C start symbol
  
 	
-	int time_a = 10000;  // Adjust the timeout value as needed
+	int time_a = 100000;  // Adjust the timeout value as needed
  	while (!(I2C1->SR1 & I2C_SR1_SB) && time_a--); // Wait for start to complete 
 	if (time_a <= 0) {
     return -1;  // error code
 	} 
 	
-	int time_b = 1000;
+	int time_b = 100000;
 	char volatile res; 
 	I2C1->DR = 0x32; // Write to data register, read/write set to 0
 	while (!(I2C1->SR1 & I2C_SR1_ADDR) && time_b--); // Wait until address sent
+	if (time_b <= 0) {
+    return -2;  // error code
+	} 
 	res = (I2C1->SR2); // Dummy read of SR2 
 	
-	I2C1->DR = 0x50; 
+	int time_c = 100000;
+	I2C1->DR = 0x28; 
 	while (!(I2C1->SR1 & (1<<7))); // Wait until TxE bit set 
+	if (time_c <= 0) {
+    return -3;  // error code
+	} 
 	I2C1->CR1 |= I2C_CR1_STOP; // Send the I2C stop symbol
 	
 	I2C1->CR1 |= I2C_CR1_START; // Send the I2C start symbol
@@ -27,7 +34,7 @@ int acceldataread(){
 	
 	
 	I2C1->DR = 0x33; // Read from data register, read/write set to 1
-	int volatile rA;
+	int rA=0;
 	
 	while (!(I2C1->SR1 & I2C_SR1_RXNE));
 	
@@ -37,3 +44,20 @@ int acceldataread(){
 	
 	return rA;
 }
+
+void acceltest(){
+	int value;
+	value = acceldataread();
+	if (value==-1){
+		GPIOB->ODR |= (1 << 4);
+	} else if(value==-2){
+		GPIOB->ODR |= (1 << 5);
+	} else if(value>0){
+	  GPIOB->ODR |= (1 << 3);
+	}
+}
+
+	
+	
+	
+	
